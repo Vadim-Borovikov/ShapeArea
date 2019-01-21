87fe58cc277924af3e29073d0480583a18e88292
Vadim Borovikov <vadim.borovikov@gmail.com>
2019-01-21 14:18:11 +0300

    API and tests classes

---
+++ ShapeArea.Tests/AreaCalculatorTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ShapeArea.Tests
+{
+    [TestClass]
+    public class AreaCalculatorTests
+    {
+        [TestMethod]
+        public void CalculateCircleAreaTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void CalculateTriangleAreaTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ ShapeArea/AreaCalculator.cs
@@ -0,0 +1,15 @@
+﻿namespace ShapeArea
+{
+    public static class AreaCalculator
+    {
+        public static double CalculateCircleArea(double radius)
+        {
+            return double.NaN;
+        }
+
+        public static double CalculateTriangleArea(double side1, double side2, double side3)
+        {
+            return double.NaN;
+        }
+    }
+}
